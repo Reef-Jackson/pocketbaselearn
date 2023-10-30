0bea2fcf0e83b3f80ff4e0c2f8cc8016a3e06d2e
Reef-Jackson <reefworkmail@gmail.com>
2023-10-30 04:33:53 +0000

    env fix

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x',
-    }),
+    adapter: adapter(),
   },
 
   preprocess: [vitePreprocess({})],
