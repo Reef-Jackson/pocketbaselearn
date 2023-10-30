5b848e7f6ad6c320a0a7dc66da2cb3fe5f276f5b
Reef-Jackson <reefworkmail@gmail.com>
2023-10-30 03:52:03 +0000

    Vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,17 @@
   import "../app.postcss";
 </script>
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"> 
 
 <div class="bg-neutral text-neutral-content">
-  <div class="max-w-wl mx-auto navbar">
-    <a href="" class="btn btn-ghost text-xl">BucketList</a>
+  <div class="mx-auto navbar">
+    <a href="/" class="btn btn-ghost text-xl">BucketList 🪸</a>
+    <a href="https://github.com/Reef-Jackson/pocketbaselearn" target="_blank" class="btn btn-ghost text-xl"> <i class="devicon-github-original text-4xl"></i></a>
     <div class="navbar-end">
       <ul class="menu menu-horizontal">
-        <li><a href="">Login</a></li>
-        <li><a href="">Register</a></li>
-        <li><a href="">About this Project</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/logout">Logout</a></li>
       </ul>
     </div>
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<a href="/logout">Logout</a>
+<!-- <a href="/logout">Logout</a> -->
+
+<div class="w-screen h-screen bg-neutral-900 flex items-center justify-center">
+    <div class="w-2/6 h-fit px-28 m-auto">
+        <h1 class="text-3xl font-bold pb-4">Bucketlist Svelte</h1>
+        <p>A todo list made with SvelteKit and Tailwind with daisyUI components.</p>
+        <p>I made this project to teach myself how HTTP requests and SvelteKit play together as my main University project is using the same stack.</p>    
+        <p>It also really helped learning Tailwind as I'm finding it's the best option for a quick, but presentable, UI.</p>    
+        <p>You can go ahead and register and add your own todo list that's exclusive to your account, all handled through Pocketbase AuthStores.</p>
+        <p>This site is hosted with Vercel and the PocketBase is hosted on a VPS running through Cloudflare.</p>
+    </div>
+
+</div>
+
+<style>
+    p {
+        @apply py-2;
+    }
+</style>
